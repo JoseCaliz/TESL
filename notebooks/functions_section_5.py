da9211f8a9f5a51da75bf77ab4b74e49d0dbfd63
JC <acalizv@gmail.com>
2021-10-31 11:13:03 -0500

    adding 5.10

--- notebooks/functions_section_5.py
+++ notebooks/functions_section_5.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 from sklearn.base import BaseEstimator, ClassifierMixin, TransformerMixin
 from sklearn.utils.validation import check_X_y, check_array, check_is_fitted        
+from scipy.optimize import minimize
 from collections.abc import Iterable
 
 def validate_X_for_spline(X) -> np.ndarray:
@@ -35,8 +36,8 @@ class global_linear_base():
         X = validate_X_for_spline(X)
         results = [np.array([1]*X.shape[0]), X]
         return np.array(results).T
-    
-    
+
+
 class global_polinomial_base():
     '''
     Global Polinomial Base that returns basis matrix
@@ -47,7 +48,7 @@ class global_polinomial_base():
         X = validate_X_for_spline(X)
         results = [np.array([1]*X.shape[0]), X, X**2, X**3]
         return np.array(results).T
-    
+
 class cubic_spline_base():
     '''
     Cubic Spline Base that returns basis matrix
@@ -75,9 +76,24 @@ class natural_cubic_spline_base():
     [1, X, X**2, X**3, d_{k_0}(X) - d_{k_{K-1}}(X), ..., d_{k_{K-2}}(X) - d_{k_{K-1}}(X)]
     '''
     
-    knots = list()
+    def integral_result(self, zeta_i, zeta_j, zeta_K_1, zeta_K):
+        A = max(zeta_j, zeta_i)
+        return(
+            -6*(
+                2*A**3 - 3*A**2*zeta_i - 3*A**2*zeta_j +
+                6*A*zeta_i*zeta_j - 2*zeta_K*zeta_i*zeta_j +
+                2*zeta_K*zeta_i*zeta_K_1 + 2*zeta_K*zeta_j*zeta_K_1 -
+                2*zeta_K*zeta_K_1**2 - 4*zeta_i*zeta_j*zeta_K_1+
+                zeta_i*zeta_K_1**2 + zeta_j*zeta_K_1**2
+            )/(
+                (zeta_K - zeta_i)*(zeta_K-zeta_j)
+            )
+        )
+    
     def __init__(self, knots:np.array):
         self.knots = knots
+        self.basis_matrix = None
+        self.omega = None
         
     def d_k(self, X, counter):
         return  (
@@ -93,4 +109,20 @@ class natural_cubic_spline_base():
         for i, knot in enumerate(self.knots[:-2]):
             results.append(self.d_k(X, i) - self.d_k(X, -2))
             
+        self.basis_matrix = np.array(results).T
         return np.array(results).T
+    
+    def generate_omega_matrix(self, X) -> np.ndarray:
+        
+        omega = np.zeros((len(X), len(X)))
+        omega[:2, :] = 0
+        omega[:, :2] = 0
+        
+        for i in range(len(X)-2):
+            for j in range(len(X)-2):
+                omega[i+2, j+2] = self.integral_result(
+                    self.knots[i], self.knots[j], self.knots[-2], self.knots[-1]
+                )
+                
+        self.omega = omega
+        return omega
